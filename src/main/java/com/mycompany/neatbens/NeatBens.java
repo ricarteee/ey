ab1ab07bdf131c86dadd58fe6cee6e5da5f04cb1
ricarteee <florenzricarte19@gmail.com>
2024-05-30 23:13:17 +0800

    Add files via upload

---
+++ src/main/java/com/mycompany/neatbens/NeatBens.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.neatbens;
+
+/**
+ *
+ * @author WINDOWS10
+ */
+public class NeatBens {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
